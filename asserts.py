e82dd2a5c4fd4ad4ccc540046876047eacbf65ae
Чегисова Ольга <chegisovaolga@gmail.com>
2025-07-17 15:18:16 +0500

    Fixes in asserts after review

--- asserts.py
+++ asserts.py
@@ -1,10 +1,3 @@
-import requests
-from models import Post
-
-
-new_post = Post(userId=1, title='Qwerty', body='Zxcvb')
-base_url = 'https://jsonplaceholder.typicode.com'
-
 def check_status_code_success(response):
     assert response.status_code == 200, f'Status code is not 200, it is {response.status_code}'
 
@@ -14,11 +7,11 @@ def check_status_code_created(response):
 def check_type_list(response):
     assert isinstance(response.json(), list), f'Type is not list, it is {type(response.json())}'
 
-def check_post_struct():
-    assert 'userId' in requests.get(f'{base_url}/posts').json()[0] and 'id' in requests.get(f'{base_url}/posts').json()[0] and 'title' in requests.get(f'{base_url}/posts').json()[0] and 'body' in requests.get(f'{base_url}/posts').json()[0], f'Item is not in {requests.get(f"{base_url}/posts").json()[0]}'
+def check_post_struct(response):
+    assert 'userId' in response.json()[0] and 'id' in response.json()[0] and 'title' in response.json()[0] and 'body' in response.json()[0], f'Item is not in {response.json()[0]}'
 
-def check_post_created(response):
-    assert 'id' in response.json() and response.json()['userId'] == new_post.userId and response.json()['title'] == new_post.title and response.json()['body'] == new_post.body, 'Data does not exist'
+def check_post_created(response, post):
+    assert 'id' in response.json() and response.json()['userId'] == post.userId and response.json()['title'] == post.title and response.json()['body'] == post.body, 'Data does not exist'
 
 def check_post_changed(response):
     assert response.json()['title'] == 'Qwerty' and response.json()['body'] == 'Zxcvb' and 'userId' in response.json(), f'Data has not been updated, {response.json()}'
--- test_api_users.py
+++ test_api_users.py
@@ -1,20 +1,22 @@
 import pytest
 import requests
+from models import Post
 from asserts import *
 
 
-
+base_url = 'https://jsonplaceholder.typicode.com'
+new_post = Post(userId=1, title='Qwerty', body='Zxcvb')
 
 def test_get():
     response_get = requests.get(f'{base_url}/posts')
     check_status_code_success(response_get)
     check_type_list(response_get)
-    check_post_struct()
+    check_post_struct(response_get)
 
 def test_post():
     response_post = requests.post(f'{base_url}/posts', json=new_post.model_dump())
     check_status_code_created(response_post)
-    check_post_created(response_post)
+    check_post_created(response_post, new_post)
 
 def test_put():
     response_put = requests.put(f'{base_url}/posts/1', data=new_post.model_dump())
